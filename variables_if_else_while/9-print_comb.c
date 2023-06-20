27ce058911818de38f9ab8bbf02c50f537b587b6
Holberton1990 <ersida9019@gmail.com>
2023-06-20 00:40:48 -0700

    9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,15 +9,15 @@
 int main(void)
 {
 int i;
-
-for (i = 0 ; i < 10 ; i++)
+{
+for (i = 0 ; i <= 9 ; i++)
 putchar((i % 10) + '0');
 if (i == 9)
 continue;
 
 putchar(',');
 putchar(' ');
-
+}
 putchar('\n');
 
 return (0);
