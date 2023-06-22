3eabb3bd1981125c310402417f9c194ed933a1d8
Holberton1990 <ersida9019@gmail.com>
2023-06-22 13:30:57 -0700

    6-print_line.c

---
+++ more_functions_nested_loops/6-main.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_line - main function
+ * @n: parameter
+ * Return: 1 if uppercase and 0 if false
+ */
+
+void print_line(int n)
+{
+	int i j;
+
+	for (i = 0, i < n, i++)
+	{
+		for (j = 0, j < n, j++)
+		{
+			if(j==i)
+				_putchar('_')
+			else 
+				_putchar(' ')
+		}
+	}
+	_putchar('\n');
+}
