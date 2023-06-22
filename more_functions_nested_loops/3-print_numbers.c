438d0a7ff60f2bd5919640ed3512a350ed7723b5
Holberton1990 <ersida9019@gmail.com>
2023-06-22 05:04:14 -0700

    The numbers speak for themselves

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * mul - main function
+ * @a: first parameter
+ * @b: second parameter
+ * Return: 1 if uppercase and 0 if false
+ */
+
+void print_numbers(void);
+{       
+       	int i;
+
+	for (i >= '0'; i < '10'; i++ )
+
+	_putchar (i);
+}
