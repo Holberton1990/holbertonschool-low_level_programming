b92814686a5e186ffa48f14b2dfd61fcc3e8300f
Holberton1990 <ersida9019@gmail.com>
2023-06-27 04:56:40 -0700

    function that compares two strings

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -9,14 +9,18 @@
 char *_strcat(char *dest, char *src)
 {
 	int i = 0;
-	int j;
+	int j = 0;
 
 	while (dest[i] != '\0')
 		i++;
 
-	for (j = 0; src[j] != '\0'; j++)
-		dest[i + j] = src[j];
-	dest[i + j] = '\0';
+	while (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+	        i++;
+		j++;
+        }
+	dest[i] = '\0';
 	return (dest);
 
 
