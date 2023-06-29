cdeed5abde0c12438a783c2b8591f371456c77cb
Holberton1990 <ersida9019@gmail.com>
2023-06-29 04:19:29 -0700

    function that fills memory with a constant byte

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -4,8 +4,28 @@
  *puts_half - function that prints half of a string
  *@str: string
  */
-void puts_half(char *str)
+char *leet(char *str)
 {	int i;
+
+	for (int i = 0; str[i] != '\0'; i++;)
+	{
+		if 
+			
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	if (strlen(str) % 2 == 0)
 		i = (strlen(str) / 2);
