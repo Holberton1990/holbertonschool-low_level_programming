b4a5a7002a8a70212f26d52b114d5be3b47d9313
Holberton1990 <ersida9019@gmail.com>
2023-06-30 02:21:34 -0700

    function that gets the length of a prefix substring.

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,20 +1,37 @@
 #include "main.h"
-/**
- *_strncpy -  function that copies a string
- *@dest: destination
- *@src: src
- *@n: n bytes to be copied
- *Return: return copied string
- */
-char *_strncpy(char *dest, char *src, int n)
-{
-	int i;
+#include <string.h>
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-	for ( ; i < n; i++)
-	dest[i] = '\0';
-	return (dest);
+/**
+ * _strspn - function that gets the length of a prefix substring
+ *@s: string to be searched
+ *@accept: characters to be matched
+ *Return: the number of bytes in the initial segment s which consist only
+ * of bytes from accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+	int found;
+
+	for (i = 0; i < (unsigned int) strlen(s); i++)
+	{
+		found = 0;
+
+		for (j = 0; j < (unsigned int) strlen(accept); j++)
+		{
+			if (s[i] == accept[j])
+			{
+				found = 1;
+				break;
+			}
+		}
+		if (!found)
+			return (i);
+
+	}
+	return (i);
+
 }
 
 
