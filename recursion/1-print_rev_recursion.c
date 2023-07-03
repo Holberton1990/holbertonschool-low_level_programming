c3c1b9760625a5076030a7b54c77ab704c1853ce
Holberton1990 <ersida9019@gmail.com>
2023-07-03 04:58:24 -0700

    function that prints a string in reverse.

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,15 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _puts_recursion - function that prints a string, reverse by a new line.
- * @s: string
+ *_print_rev_recursion - function that prints a string reverse
+ *@s: string
  */
 
 void _print_rev_recursion(char *s)
 {
-	if(*s == '\0')
-	       return;
-	_print_rev_recursion(s+1);
+	if (*s == '\0')
+		return;
+	_print_rev_recursion(s + 1);
 	_putchar(*s);
 }
 
