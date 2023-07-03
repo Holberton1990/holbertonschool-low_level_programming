d3107cf38287ac937510ead7103989e85b898cb9
Holberton1990 <ersida9019@gmail.com>
2023-07-03 05:23:08 -0700

    function that prints that return the length of string.

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
-#include <stdio.h>
 /**
- *_print_rev_recursion - function that prints a string reverse
- *@s: string
+ *_strlen_recursion - function that returns the length of a string
+ *@s: input
+ *Return: return
  */
 
 int _strlen_recursion(char *s)
