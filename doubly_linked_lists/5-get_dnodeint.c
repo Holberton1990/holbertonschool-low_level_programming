662ea9914fa9aa6ada60b94ef68392002be47330
Holberton1990 <ersida9019@gmail.com>
2023-08-02 01:47:39 -0700

    function that returns the nth node of a dlistint_t linked list.

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -14,22 +14,21 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
     unsigned int i;
     dlistint_t *it = head;
 
-    // Kontrolli për rastin e listës bosh
+    
     if (head == NULL)
         return NULL;
 
-    // Loop-i për të gjetur nyjen në indeksin e dhënë
+    
     for (i = 0; i < index; i++)
     {
-        // Kontrolli për rastin kur jemi jashtë kufijve të listës
+        
         if (it == NULL)
             return NULL;
 
-        // Zhvendosemi te nyja tjetër
+        
         it = it->next;
     }
 
-    // Kthejmë nyjen në indeksin e caktuar
     return it;
 }
 
