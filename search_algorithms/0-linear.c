d37329f79ce84cd9efd51389737abd170d1546cc
Holberton1990 <ersida9019@gmail.com>
2023-09-18 13:10:16 -0700

    sorty

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -2,16 +2,19 @@
 #include <stddef.h>
 #include "search_algos.h"
 
-/**
- * linear_search - Kërkon një vlerë në një varg të numrave të plotë
- * @array: Vargu ku kërkojmë
- * @size: Madhësia e vargut
- * @value: Vlera që kërkojmë
+ /**
+ * linear_search - Searches for a value in an array of integers
+ * @array: The array to search in
+ * @size: The size of the array
+ * @value: The value to find
  *
- * Kërkon vlerën @value në vargun @array me madhësi @size duke përdorur
- * algoritmin e kërkimit lineare. Kthehet -1 nëse vlera nuk gjendet ose vargu
- * është NULL, ose kthen indeksin e vlerës në varg nëse gjendet.
+ * Searches for @value in the @array using the linear search algorithm.
+ * Returns the index of the value if found, or -1 if it's not present in the array
+ * or if the array is NULL. Prints each checked value during the search.
+ *
+ * Return: If the value is found, returns the index; otherwise, returns -1.
  */
+
 int linear_search(int *array, size_t size, int value) 
 {
     size_t i;
