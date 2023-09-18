1a0ef3fcd6c6f7f1b1355ef0f9dae5124ca24442
Holberton1990 <ersida9019@gmail.com>
2023-09-18 12:35:29 -0700

    sorty

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,19 +1,18 @@
-#include "search_algos.h"
 #include <stdio.h>
 #include <stddef.h>
-/*
- *
- * */
-int linear_search(int *array, size_t size, int value)	
-	if (array == NULL) {
-		   return -1;
-	   }
-   }	
-	for (size_i = 0; i < size; i++) {
-   		printf("Value checked array[%lu] = %d\n", i, array
-				if (array[i] == value){
-				return i;
-				}
-				}
-		return -1;
-   }
+#include "search_algos.h"
+int linear_search(int *array, size_t size, int value) {
+    if (array == NULL) {
+        return -1; // Kthe -1 nëse array është NULL
+    }
+
+    for (size_t i = 0; i < size; i++) {
+        printf("Value checked array[%lu] = %d\n", i, array[i]);
+        if (array[i] == value) {
+            return i; // Kthe indeksin nëse vlera është gjetur
+        }
+    }
+
+    return -1; // Kthe -1 nëse vlera nuk është gjetur në array
+}
+
