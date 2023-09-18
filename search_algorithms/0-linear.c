63a34b81edd02e861f7c4add140906d2764f6a0d
Holberton1990 <ersida9019@gmail.com>
2023-09-18 12:11:28 -0700

    sorty

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,18 @@
+#include "search_algos.h"
+#include <stdio.h>
+/*
+ *
+ * */
+int linear_search(int *array, size_t size, int value)	
+	if (array == NULL) {
+		   return -1;
+	   }
+   }	
+	for (size_i = 0; i < size; i++) {
+   		printf("Value checked array[%lu] = %d\n", i, array
+				if (array[i] == value){
+				return i;
+				}
+				}
+		return -1;
+   }
