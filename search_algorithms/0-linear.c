244c0ab949351d0e15dda13e5dde45fdc4c6b427
Holberton1990 <ersida9019@gmail.com>
2023-09-18 13:00:36 -0700

    sorty

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -3,27 +3,27 @@
 #include "search_algos.h"
 
 /**
- *linear_search - function that searches for a value in an array of integers
- *@array: array to be searched
- *@size: size of the array
- *@value: value to find
- *Return: -1 if its not present or array is NULL or the index of the value
- *if found
+ * linear_search - function that searches for a value in an array of integers
+ * @array: array to be searched
+ * @size: size of the array
+ * @value: value to find
+ * Return: -1 if it's not present or array is NULL or the index of the value
+ * if found
  */
 
 int linear_search(int *array, size_t size, int value) 
 {	
-	int i;
+    size_t i;
 
-    	if (array == NULL) 
+    if (array == NULL) 
         return -1;
-    
+
     for (i = 0; i < size; i++) 
     {
         printf("Value checked array[%lu] = %d\n", i, array[i]);
         if (array[i] == value) 
-            return i; 
-        }
+            return (int)i; 
+    }
     return -1;
 }
 
