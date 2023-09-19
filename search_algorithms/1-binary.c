9a4a43512d6b037490caf08ae67d85c83376b947
Holberton1990 <ersida9019@gmail.com>
2023-09-19 02:29:01 -0700

    binary

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -5,7 +5,7 @@
  *
  */
 int binary_search(int *array, size_t size, int value) {
-    size_t low, high, i; /* Declare variables at the beginning.*/
+    size_t low, high, i; /* Declare variables at the beginning. */
 
     if (array == NULL || size == 0) {
         return -1;
@@ -17,14 +17,14 @@ int binary_search(int *array, size_t size, int value) {
     while (low <= high) {
         size_t mid = (low + high) / 2;
 
-        printf("Searching in array[%u:%u] = {", (unsigned)low, (unsigned)high); 
+        printf("Searching in array: ");
         for (i = low; i <= high; i++) {
             printf("%d", array[i]);
             if (i < high) {
                 printf(", ");
             }
         }
-        printf("}\n");
+        printf("\n");
 
         if (array[mid] == value) {
             return mid;
