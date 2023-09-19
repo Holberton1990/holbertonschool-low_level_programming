ab9a2d8e70c7e5a5406eb05d932955002ae990d3
Holberton1990 <ersida9019@gmail.com>
2023-09-19 02:17:20 -0700

    binary

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,35 +1,38 @@
 #include "search_algos.h"
 #include <stdio.h>
+
 /*
  *
  */
-int binary_search(int *array, size_t size, int value)
-{
-	if (array == NULL || size == 0) {
-		return -1;
-	}
+int binary_search(int *array, size_t size, int value) {
+    if (array == NULL || size == 0) {
+        return -1;
+    }
 
-	size_t low = 0;
-	size_t high = size - 1;
+    size_t low = 0;
+    size_t high = size - 1;
+    size_t i; 
 
-	while (low <= high) {
-		size_t mid = (low + high)/2;
+    while (low <= high) {
+        size_t mid = (low + high) / 2;
 
-		printf("Searching in array[%d:%d] = {", low, high);
-		for (size_t i = low; i <= high; i++) {
-			printf("%d",array[i]);
-			if (i < high) {
-				printf(", ");
-			}
-		}
-		printf("}\n");
+        printf("Searching in array[%zu:%zu] = {", low, high); 
+        for (i = low; i <= high; i++) {
+            printf("%d", array[i]);
+            if (i < high) {
+                printf(", ");
+            }
+        }
+        printf("}\n");
 
-		if (array[mid] == value){
-			return mid;
-		}else if (array[mid] < value){
-			low = mid +1;
-		}else {
-			high = mid -1;
-		}
-	}
-	return -1;
+        if (array[mid] == value) {
+            return mid;
+        } else if (array[mid] < value) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
+
