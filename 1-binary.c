b3b9b1444077a4f5b2c5ce19f83b1696d81dfc4b
Holberton1990 <ersida9019@gmail.com>
2023-09-19 02:06:04 -0700

    binary

---
+++ 1-binary.c
@@ -0,0 +1,35 @@
+#include "search_algos.h"
+#include <stdio.h>
+/*
+ *
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	if (array == NULL || size == 0) {
+		return -1;
+	}
+
+	int low = 0;
+	int high = size - 1;
+
+	while (low <= high) {
+		int mid = (low + high)/2;
+
+		printf("Searching in array[%d:%d] = {", low, high);
+		for (size_t i = low; i <= high; i++) {
+			printf("%d",array[i]);
+			if (i < high) {
+				printf(", ");
+			}
+		}
+		printf("}\n");
+
+		if (array[mid] == value){
+			return mid;
+		}else if (array[mid] < value){
+			low = mid +1;
+		}else {
+			high = mid -1;
+		}
+	}
+	return -1;
