8612befed99209a845af9ae25c8d45d7b34b8982
Holberton1990 <ersida9019@gmail.com>
2023-09-19 02:23:01 -0700

    binary

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -5,13 +5,14 @@
  *
  */
 int binary_search(int *array, size_t size, int value) {
+    size_t low, high, i; // Declare variables at the beginning.
+
     if (array == NULL || size == 0) {
         return -1;
     }
 
-    size_t low = 0;
-    size_t high = size - 1;
-    size_t i; 
+    low = 0;
+    high = size - 1;
 
     while (low <= high) {
         size_t mid = (low + high) / 2;
